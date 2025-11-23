4767b8acfdbd5846f9ce42085dd3e1fb0d082a40
Durjustice <deathwhale@163.com>
2025-11-23 23:58:03 +0800

    impl sys_trace

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -3,6 +3,7 @@ use crate::{
     task::{exit_current_and_run_next, suspend_current_and_run_next},
     timer::get_time_us,
 };
+use crate::task::get_syscall_count;
 
 #[repr(C)]
 #[derive(Debug)]
@@ -41,5 +42,10 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 // TODO: implement the syscall
 pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
     trace!("kernel: sys_trace");
-    -1
+    match _trace_request {
+        0 => unsafe { (_id as *const u8).read_volatile() as isize },
+        1 => unsafe { (_id as *mut u8).write_volatile(_data as u8); 0 },
+        2 => get_syscall_count(),
+        _ => -1,
+    }
 }
--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -45,6 +45,8 @@ pub struct TaskManagerInner {
     tasks: [TaskControlBlock; MAX_APP_NUM],
     /// id of current `Running` task
     current_task: usize,
+    /// syscall count for each app
+    syscall_count: [isize; MAX_APP_NUM],
 }
 
 lazy_static! {
@@ -65,6 +67,7 @@ lazy_static! {
                 UPSafeCell::new(TaskManagerInner {
                     tasks,
                     current_task: 0,
+                    syscall_count: [0; MAX_APP_NUM],
                 })
             },
         }
@@ -135,6 +138,20 @@ impl TaskManager {
             panic!("All applications completed!");
         }
     }
+
+    /// Increment syscall count for current task
+    fn increment_syscall_count(&self) {
+        let mut inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.syscall_count[current] += 1;
+    }
+
+    /// Get syscall count for current task
+    fn get_syscall_count(&self) -> isize {
+        let inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+        inner.syscall_count[current]
+    }
 }
 
 /// Run the first task in task list.
@@ -168,4 +185,14 @@ pub fn suspend_current_and_run_next() {
 pub fn exit_current_and_run_next() {
     mark_current_exited();
     run_next_task();
+}
+
+/// Increment syscall count for current task
+pub fn increment_syscall_count() {
+    TASK_MANAGER.increment_syscall_count();
+}
+
+/// Get syscall count for current task
+pub fn get_syscall_count() -> isize {
+    TASK_MANAGER.get_syscall_count()
 }
